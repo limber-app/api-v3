ce094880775024284f16fa5e0ffe028ba9ae8a9b
quarkusio <no-reply@quarkus.io>
2021-04-26 09:27:51 +0000

    Initial commit

---
+++ src/test/java/io/limber/NativeSpringGreetingControllerIT.java
@@ -0,0 +1,9 @@
+package io.limber;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSpringGreetingControllerIT extends SpringGreetingControllerTest {
+
+    // Execute the same tests but in native mode.
+}
